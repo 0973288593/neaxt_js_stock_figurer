12a3d7cca6765806bc64cf1356f80aa5fd277051
kongsak-22028 <117248090+kongsak-22028@users.noreply.github.com>
2025-08-24 22:04:12 +0700

    เพิ่ม script insert admin คนเเรก

---
+++ prisma/seed.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from '@prisma/client'
+// const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.person.createMany({
+    data: [
+      {
+        LastName: "Admin",
+        FirstName: "Super",
+        Address: "123 Main St",
+        City: "Bangkok",
+        Email: "admin@admin.com",
+        Password: "admin" // 🚨 ควร hash จริงๆ นะครับ
+      },
+    ],
+    skipDuplicates: true, // กัน insert ซ้ำ
+  })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
