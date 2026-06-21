950645cf2a1c15f17c44a2ec3d8d928d17010c60
kongsak-22028 <117248090+kongsak-22028@users.noreply.github.com>
2026-06-21 12:45:37 +0700

    เเก้ ซ้อน sidbar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import {
   LucideIcon,
   LayoutDashboard,
@@ -10,9 +10,8 @@ import {
   HandCoins
 } from "lucide-react";
 
-import { SidebarItems } from "@/types";
-import SidebarItem from "./item";
 
+import SidebarItem from "./item";
 interface ISidebarItem {
   name: string;
   path: string;
@@ -25,6 +24,9 @@ interface ISubItem {
   path: string;
 }
 
+const returnSidbar = () => {
+
+}
 const items: ISidebarItem[] = [
   {
     name: "Dashboard",
@@ -73,24 +75,47 @@ const items: ISidebarItem[] = [
 ];
 
 const Sidebar = () => {
+const [isOpen, setIsOpen ] = useState<boolean>(true);
+const [ripple, setRipple] = useState<boolean>(false);
+const handIeLogoClick = (): void => {
+  setRipple(true);
+  setTimeout(() =>{
+    setRipple(false);
+    setIsOpen((prev) => !prev);
+  },400)
+ 
+}
+
+
   return (
-    <div className="fixed top-0 left-0 h-screen w-64 bg-white shadow-lg z-10 p-4">
-      <div className="flex flex-col spece-y-10 w-full">
+    <div className={`fixed top-0 left-0 h-screen  transition-all duration-300 ${ isOpen ? "w-64 p-4  bg-white shadow-lg z-10 " : "w-16 p-2"}`}>
+      {/* <div className="flex flex-col spece-y-10 w-full"> */}
+      <div className="flex flex-col space-y-10 w-fill">
+
         <div className="flex" style={{ alignItems: "center" }}>
           <img
             style={{ width: "50px" }}
             className="h-100 w-fil rounded-full"
             src="/images/logo.png"
+            onClick={handIeLogoClick}
           />
-          <h3 className="text-black ml-3" style={{ width: "100%" }}>
-            Model
-          </h3>
+          {ripple && (
+            <span className="absolute w-12 h-12 rounded-full aninmate-rapple border-2 border-blue-400"></span>
+          )}
+       
+          {isOpen && (  
+            <h3 className="text-black ml-3">
+            Model 
+          </h3>)}
+
         </div>
-        <div className="flex flex-col space-y-2 mt-2">
+        {isOpen && (
+           <div className="flex flex-col space-y-2 mt-2">
           {items.map((item, index) => (
             <SidebarItem key={index} item={item} />
           ))}
         </div>
+        )}
       </div>
     </div>
   );
